86457d2ba2bc6ee2572471f252a96dea8d5aa9b9
haotq2003 <145550790+haotq2003@users.noreply.github.com>
2025-04-01 11:54:39 +0700

    Revert "Hao"

--- eStore/Program.cs
+++ eStore/Program.cs
@@ -1,7 +1,7 @@
-﻿
-
+using DataAccess.Data;
 using BusinessObject.Entities;
-
+using DataAccess.Repositories;
+using DataAccess.Repositories.Interfaces;
 using eStore.Components;
 using eStore.Services.Interfaces;
 using eStore.Services;
@@ -10,56 +10,38 @@ using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using eStore.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using DataAccess.Data;
-using DataAccess.Repositories.Interfaces;
-using DataAccess.Repositories;
-using Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// Database configuration
-var conString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<eStoreDuckContext>(options =>
-    options.UseSqlServer(conString));
-
-// Admin account settings
+var conString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<eStoreDuckContext>(options => options.UseSqlServer(conString));
 builder.Services.Configure<AdminAccountSettings>(builder.Configuration.GetSection("AdminAccount"));
-
-// Repository and Service registrations
 builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddScoped<IMemberService, MemberService>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
-builder.Services.AddScoped<IOrderDetailService, OrderDetailService>();
-
-// Authentication and Authorization
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<ProtectedLocalStorage>();
 builder.Services.AddAuthentication(options =>
 {
-    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 })
-.AddCookie(options =>
-{
-    options.Cookie.Name = "BlazorAuthCookie";
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromDays(1);
-    options.LoginPath = "/login";
-    options.AccessDeniedPath = "/access-denied";
-    options.SlidingExpiration = true;
-});
-
-// Distributed Cache and Session
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "BlazorAuthCookie";
+        options.Cookie.HttpOnly = true;
+        options.ExpireTimeSpan = TimeSpan.FromDays(1);
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/";
+        options.SlidingExpiration = true;
+    });
+// Configure Distributed Cache (for session persistence across SignalR connections)
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddDistributedMemoryCache();
+// Add Session
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
@@ -67,31 +49,23 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
-// Additional services
-//builder.Services.AddQuickGridEntityFrameworkAdapter();
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseSession(); // Phải trước UseAuthentication để session hoạt động trước khi xác thực
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseAntiforgery();
+app.UseSession();
 
-// Middleware để chuyển hướng từ "/" về "/login"
 app.Use(async (context, next) =>
 {
     if (context.Request.Path == "/")
@@ -99,7 +73,7 @@ app.Use(async (context, next) =>
         context.Response.Redirect("/login");
         return;
     }
-    await next.Invoke();
+    await next();
 });
 
 app.MapRazorComponents<App>()
